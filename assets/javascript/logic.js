a1ac384f27f2aeb2980b7d5b559604f6c9e62306
B-Helgeson <brandonstevenhelgeson@gmail.com>
2018-03-04 12:13:29 -0500

    inital html build and base javascript

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,37 @@
-window.onload = function() { //Loads up the JavaScript after the page finishes loading
+$(document).ready(function() { //Loads up the JavaScript after the page finishes loading
+
+//defining variables used on page
+    var targetNum = 1;
+    var currentNum = 2;
+    var winCount = 3;
+    var lossCount = 4;
+
+//function which can be called to refresh the variables on the page at the end of each click event
+    function refreshPageVars(){
+    $("target").html(targetNum.toString());
+    $("current").html(currentNum);
+    $("wins").html(winCount);
+    $("losses").html(lossCount);
+    }
+
+    
+refreshPageVars();
+
+    
+    //for (var i = 0; i < letters.length; i++) {
+      //  var letterBtn = $("<button class = 'letter-button letter letter-button-color'>");
+        //$(letterBtn).attr("data-letter", letters[i]);
+        //$(letterBtn).text(letters[i]);}
+
+
+        //writes intial guesses left value on the DOM
+//document.getElementById('winCount').innerHTML = wCount;
+//writes intial Losses value on the DOM
+//document.getElementById('lossCount').innerHTML = lCount;
+//writes intial Wins value on the DOM
+//document.getElementById('guess').innerHTML = gCount;
+
+
 
 
 
@@ -40,4 +73,4 @@ window.onload = function() { //Loads up the JavaScript after the page finishes l
 
     console.log("All Logic Processed")//Test to ensure that all of the above javascript worked. 
 
-} //closing the on load function, this should be the last line of js
+}); //closing the on load function, this should be the last line of js
