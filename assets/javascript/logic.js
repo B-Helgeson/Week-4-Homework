536aa74be5b8e502794106d50cf8668915df737c
B-Helgeson <brandonstevenhelgeson@gmail.com>
2018-03-09 22:11:50 -0500

    updates

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -28,24 +28,9 @@ $(document).ready(function() { //Loads up the JavaScript after the page finishes
 //function to generate random crystal values for c1 - c4
         //for loop on crystals array, math floor between 1 - 12.
 
-    function generateCrystals() {
 
-        var crystalImages = [
-            "assets/images/crystalRuby.png",
-            "assets/images/crystalEmerald.png",
-            "assets/images/crystalSapphire.png",
-            "assets/images/crystalDiamond.png"];
 
-            for (var i = 0; i < crystalImages.length; i++) {
-                var crystal = $('<img src="'+ crystalImages[i]+'">')
-                $(crystal).attr("data-random")
-                $(crystal).appendTo("#crys")
-            }
-
-    }
-
-generateCrystals();
-
+        
 //on click listener to add crystal values to the current number
         $("#c1").on("click",function(){
         })
@@ -89,31 +74,3 @@ start();
     console.log("All Logic Processed")//Test to ensure that all of the above javascript worked. 
 
 }); //closing the on load function, this should be the last line of js
-
-
-
-// Reference Logic from Fellow Student
-
-// function start(){ 
-//     var crystalImages = [
-//         "assets/images/crystalRuby.png",
-//         "assets/images/crystalEmerald.png",
-//         "assets/images/crystalSapphire.png",
-//         "assets/images/crystalDiamond.png"];
-// //Creates crystals and random values and assigns it 
-//     for (var i = 0; i < 4; i++) {
-// //Assigns random value between 1-12 to Crystal
-//         var random = Math.floor((Math.random() * 11) + 1);
-// //Creates crystals on HTML and assigns the random value
-//         var crystal = $("<div>");
-//         crystal.attr({
-//             "class": 'crystal anime',
-//             "data-random": random,
-//         });
-//         crystal.css({
-//             "background-image": "url('"+ crystalImages[i]+"')",
-//             "background-size": "cover"
-//         });
-//         $(".crystals").append(crystal)
-//     }
-// }
