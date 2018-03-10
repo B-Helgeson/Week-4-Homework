305f85e895fc37355f4491d3b675ca04905463bc
B-Helgeson <brandonstevenhelgeson@gmail.com>
2018-03-09 21:03:09 -0500

    ongoing javascript development

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -5,51 +5,115 @@ $(document).ready(function() { //Loads up the JavaScript after the page finishes
     var currentNum = 0;
     var winCount = 0;
     var lossCount = 0;
-    var crytals = [ruby, emerald, sapphire, diamond]
+    var crystalPics = []
 
-//function which can be called to refresh the variables on the page at the end of each round
-    function refreshPageVars(){
+ 
+
+//function which can be called to rewrite the variables on the page at the end of each action
+    function rewritePageVars(){
     $("#current").text(currentNum);
     $("#wins").text(winCount);
     $("#losses").text(lossCount);
-    generateTargetNum();
-    $("target").text(targetNum)
+    $("#target").text(targetNum)
     }
 
 //function to generate a random target number.
-        //math random, math floor between 19 - 120.
+        function generateTargetNum() {
+        targetNum = Math.floor((Math.random() * 120) + 19);
+        console.log(targetNum)}
+
+        generateTargetNum();
 
 
 //function to generate random crystal values for c1 - c4
         //for loop on crystals array, math floor between 1 - 12.
 
+    function generateCrystals() {
 
+        var crystalImages = [
+            "assets/images/crystalRuby.png",
+            "assets/images/crystalEmerald.png",
+            "assets/images/crystalSapphire.png",
+            "assets/images/crystalDiamond.png"];
+
+            for (var i = 0; i < crystalImages.length; i++) {
+                var crystal = $('<img src="'+ crystalImages[i]+'">')
+                $(crystal).attr("data-random")
+                $(crystal).appendTo("#crys")
+            }
+
+    }
+
+generateCrystals();
 
 //on click listener to add crystal values to the current number
-
+        $("#c1").on("click",function(){
+        })
 
 
 
 //values listener to alert on win or loss and then reset the game (alert upon reset)
+        if (currentNum > targetNum) {
+            lossCount++;
+            rewritePageVars();
+            alert("You lose!")
 
+        } else if (currentNum === targetNum) {
+            winCount++;
+            reWritePageVars();
+            alert("You Win!")
+        }
 
 
 
 //initial write of page values, to be removed later. 
-    refreshPageVars();
+    rewritePageVars();
 
 
 
     //button to increment then refresh page values, just to test overall functionality... will be removed later. 
     $("#inc").click(function(){
-        targetNum++;
         currentNum++;
         winCount++;
         lossCount++;
-        refreshPageVars()});
+        rewritePageVars()
+    });
 
+
+
+
+
+start();
 
 
     console.log("All Logic Processed")//Test to ensure that all of the above javascript worked. 
 
 }); //closing the on load function, this should be the last line of js
+
+
+
+// Reference Logic from Fellow Student
+
+// function start(){ 
+//     var crystalImages = [
+//         "assets/images/crystalRuby.png",
+//         "assets/images/crystalEmerald.png",
+//         "assets/images/crystalSapphire.png",
+//         "assets/images/crystalDiamond.png"];
+// //Creates crystals and random values and assigns it 
+//     for (var i = 0; i < 4; i++) {
+// //Assigns random value between 1-12 to Crystal
+//         var random = Math.floor((Math.random() * 11) + 1);
+// //Creates crystals on HTML and assigns the random value
+//         var crystal = $("<div>");
+//         crystal.attr({
+//             "class": 'crystal anime',
+//             "data-random": random,
+//         });
+//         crystal.css({
+//             "background-image": "url('"+ crystalImages[i]+"')",
+//             "background-size": "cover"
+//         });
+//         $(".crystals").append(crystal)
+//     }
+// }
